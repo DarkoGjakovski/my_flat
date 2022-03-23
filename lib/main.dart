2d5a33e0f294b94e4e591e1ccaf5b9a01f2af2b4
FlutLab.io <>
2022-03-23 19:38:19 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -77,7 +77,7 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
           ),
         ],
         currentIndex: _selectedIndex,
-        selectedItemColor: Colors.amber[800],
+        selectedItemColor: Colors.red,
         onTap: _onItemTapped,
       ),
     );
